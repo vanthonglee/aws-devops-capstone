93838fca7dc0b0549530927d5f37cea2900c32ed
vanthonglee <thonglevan131@gmail.com>
2022-07-13 17:05:05 +0700

    .

--- infra/create-cluster.sh
+++ infra/create-cluster.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-eksctl create cluster --name my-cluster --region region-code
+eksctl create cluster \
+--name capstone-georgelee \
+--version 1.22 \
+--region us-east-1 \
+--account udacity-lab3
+--nodegroup-name cgc-linux-nodes \
+--node-type t2.micro \
+--nodes-min=1 \
+--nodes-max=3
+
+eksctl create cluster --name=capstone-georgelee --region=us-east-2
